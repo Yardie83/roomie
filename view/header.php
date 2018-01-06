6e1467d0cabf70ff71d561023287368dcac3baf5
Yardie83 <hermann.grieder@gmail.com>
2018-01-06 13:09:49 +0100

    initial views updated

--- view/header.php
+++ view/header.php
@@ -4,33 +4,29 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WE-CRM</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/navigation.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>header_loggedOut</title>
+    <link rel="stylesheet" href="header/header_loggedOut/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="header/header_loggedOut/assets/css/Header-Blue.css">
+    <link rel="stylesheet" href="header/header_loggedOut/assets/css/styles.css">
 </head>
 
 <body>
-<div>
-    <nav class="navbar navbar-default navigation-clean">
-        <div class="container">
-            <div class="navbar-header"><a class="navbar-brand navbar-link" href="<?php echo $GLOBALS["ROOT_URL"]; ?>">WE-CRM </a>
-                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+    <div>
+        <nav class="navbar navbar-default navigation-clean-search">
+            <div class="container">
+                <div class="navbar-header"><a class="navbar-brand navbar-link" href="#">Roomie </a>
+                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                </div>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav"></ul>
+                    <p class="navbar-text navbar-right"><a class="navbar-link login" href="#">Log In</a> <a class="btn btn-default action-button" role="button" href="#">Sign Up</a></p>
+                    <ul class="nav navbar-nav">
+                        <li role="presentation"><a href="#">Search </a></li>
+                    </ul>
+                </div>
             </div>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li role="presentation"><a href="<?php echo $GLOBALS["ROOT_URL"]; ?>">My Customers</a></li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">My Profile <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li role="presentation"><a href="<?php echo $GLOBALS["ROOT_URL"]; ?>/agent/edit">Edit Profile</a></li>
-                            <li role="presentation"><a href="<?php echo $GLOBALS["ROOT_URL"]; ?>/logout">Logout </a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</div>
+        </nav>
+    </div>
+    <script src="header/header_loggedOut/assets/js/jquery.min.js"></script>
+    <script src="header/header_loggedOut/assets/bootstrap/js/bootstrap.min.js"></script>
