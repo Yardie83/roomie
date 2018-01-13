0ff0b3313456ff2b40423b00f86fb5a0bc0023a6
FHNWGery <BaDru1700>
2018-01-13 13:46:06 +0100

    PDF export functionality

--- view/listingDetailPDF.php
+++ view/listingDetailPDF.php
@@ -1,62 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andreas.martin
- * Date: 13.09.2017
- * Time: 16:59
- */
 use view\TemplateView;
+
+$listing = $this->listing[0];
 ?>
-<!DOCTYPE html>
-<html>
+
 <h1 align="center" style="font-family: Helvetica;"><span style="color: #f4476b;">Roomie</span> | Your apartment</h1>
 <br>
-<table style="height: 237px; border-color: #f4476b; border-collapse: collapse; width: 463.5px; margin-left: auto; margin-right: auto; font-size: 20px;" border="1">
+<table border="0" style="height: 237px; border-color: #f4476b; border-collapse: collapse; width: 463.5px; margin-left: auto; margin-right: auto; font-size: 20px;" >
     <tbody>
-    <tr style="height: 30px; font-family: Helvetica;">
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica;">Street name</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">&nbsp;</td>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107);">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Street</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getStreet()); ?></td>
     </tr>
-    <tr style="height: 30px;">
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica;">Zip Code</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">&nbsp;</td>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107)">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Zip Code</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getPlz()); ?></td>
     </tr>
-    <tr style="height: 30px;">
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica;">City</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">&nbsp;</td>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107)">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">City</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getCity()); ?></td>
     </tr>
-    <tr style="height: 30px;">
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica;">Canton</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">&nbsp;</td>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107)">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Canton</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getCanton()); ?></td>
     </tr>
-    <tr style="height: 30px;">
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica;">Number of rooms</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">&nbsp;</td>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107)">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Number of rooms</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getNumberofrooms()); ?></td>
     </tr>
-    <tr style="height: 30px;">
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica;">Square meters</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">&nbsp;</td>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107)">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Square meters</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getSquaremeters()); ?></td>
     </tr>
-    <tr style="height: 30px;">
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica;">Rent</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">&nbsp;</td>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107)">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Rent</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getPrice()); ?></td>
     </tr>
-    <tr style="height: 30px;">
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica;">Available from</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">&nbsp;</td>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107);">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Available from</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getMoveInDate()); ?></td>
+    </tr>
+    <tr style="height: 30px; border-top: 1px solid rgb(244,71,107); border-bottom: 1px solid rgb(244,71,107)">
+        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Description</td>
+        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($listing->getDescription()); ?></td>
     </tr>
     </tbody>
 </table>
-    <?php
-    foreach($this->customers as $customer): /* @var Customer $customer */ ?>
-    <tr>
-        <td><?php echo $customer->getId(); ?> </td>
-        <td><?php echo TemplateView::noHTML($customer->getName()); ?></td>
-        <td><?php echo TemplateView::noHTML($customer->getEmail(), false); ?> </td>
-        <td><?php echo TemplateView::noHTML($customer->getMobile()); ?> </td>
-    </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
-</body>
-</html>
