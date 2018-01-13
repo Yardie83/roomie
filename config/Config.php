c57aad2d6eac56a8f0cca852bbbf9a8a39f52d5c
LorisTheBoss <loris.grether@students.fhnw.ch>
2018-01-13 23:13:16 +0100

    seach functionality with all around and attached

--- config/Config.php
+++ config/Config.php
@@ -15,31 +15,62 @@ class Config
 
     public static function init()
     {
+//        if (file_exists(self::$iniFile)) {
+//            $data = parse_ini_file(self::$iniFile, true);
+//            $databaseConfig = $data["database"];
+//            self::$config["pdo"]["dsn"] = $databaseConfig ["driver"] . ":host=" . $databaseConfig ["host"] . ";port=" . $databaseConfig["port"] . "; dbname=" . $databaseConfig ["database"] . "; sslmode=require";
+//            self::$config["pdo"]["user"] = $databaseConfig["user"];
+//            self::$config["pdo"]["password"] = $databaseConfig["password"];
+//            self::$config["email"]["sendgrid-apikey"] = $data["email"]["sendgrid-apikey"];
+//            self::$config["pdf"]["hypdf-user"] = $data["pdf"]["hypdf-user"];
+//            self::$config["pdf"]["hypdf-password"] = $data["pdf"]["hypdf-password"];
+//        } else {
+//            if (isset($_ENV["DATABASE_URL"])) {
+//                $dbopts = parse_url(getenv('DATABASE_URL'));
+//                self::$config["pdo"]["dsn"] = "pgsql" . ":host=" . $dbopts["host"] . ";port=" . $dbopts["port"] . "; dbname=" . ltrim($dbopts["path"], '/') . "; sslmode=require";
+//                self::$config["pdo"]["user"] = $dbopts["user"];
+//                self::$config["pdo"]["password"] = $dbopts["pass"];
+//            }
+//            if (isset($_ENV["SENDGRID_APIKEY"])) {
+//                self::$config["email"]["sendgrid-apikey"] = getenv('SENDGRID_APIKEY');
+//            }
+//            if (isset($_ENV["HYPDF_USER"])) {
+//                self::$config["pdf"]["hypdf-user"] = getenv('HYPDF_USER');
+//            }
+//            if (isset($_ENV["HYPDF_PASSWORD"])) {
+//                self::$config["pdf"]["hypdf-password"] = getenv('HYPDF_PASSWORD');
+//            }
+//        }
+
         if (file_exists(self::$iniFile)) {
-            $data = parse_ini_file(self::$iniFile, true);
-            $databaseConfig = $data["database"];
-            self::$config["pdo"]["dsn"] = $databaseConfig ["driver"] . ":host=" . $databaseConfig ["host"] . ";port=" . $databaseConfig["port"] . "; dbname=" . $databaseConfig ["database"] . "; sslmode=require";
-            self::$config["pdo"]["user"] = $databaseConfig["user"];
-            self::$config["pdo"]["password"] = $databaseConfig["password"];
-            self::$config["email"]["sendgrid-apikey"] = $data["email"]["sendgrid-apikey"];
-            self::$config["pdf"]["hypdf-user"] = $data["pdf"]["hypdf-user"];
-            self::$config["pdf"]["hypdf-password"] = $data["pdf"]["hypdf-password"];
-        } else {
-            if (isset($_ENV["DATABASE_URL"])) {
-                $dbopts = parse_url(getenv('DATABASE_URL'));
-                self::$config["pdo"]["dsn"] = "pgsql" . ":host=" . $dbopts["host"] . ";port=" . $dbopts["port"] . "; dbname=" . ltrim($dbopts["path"], '/') . "; sslmode=require";
-                self::$config["pdo"]["user"] = $dbopts["user"];
-                self::$config["pdo"]["password"] = $dbopts["pass"];
-            }
-            if (isset($_ENV["SENDGRID_APIKEY"])) {
-                self::$config["email"]["sendgrid-apikey"] = getenv('SENDGRID_APIKEY');
-            }
-            if (isset($_ENV["HYPDF_USER"])) {
-                self::$config["pdf"]["hypdf-user"] = getenv('HYPDF_USER');
-            }
-            if (isset($_ENV["HYPDF_PASSWORD"])) {
-                self::$config["pdf"]["hypdf-password"] = getenv('HYPDF_PASSWORD');
-            }
+        $databaseConfig = parse_ini_file(self::$iniFile, true)["database"];
+        self::$config["pdo"]["dsn"] = $databaseConfig ["driver"] . ":host=" . $databaseConfig ["host"] . ";port=" . $databaseConfig["port"] . "; dbname=" . $databaseConfig ["database"] . "; sslmode=require";
+        self::$config["pdo"]["user"] = $databaseConfig["user"];
+        self::$config["pdo"]["password"] = $databaseConfig["password"];
+    } elseif (isset($_ENV["DATABASE_URL"])) {
+        $dbopts = parse_url(getenv('DATABASE_URL'));
+        self::$config["pdo"]["dsn"] = "pgsql" . ":host=" . $dbopts["host"] . ";port=" . $dbopts["port"] . "; dbname=" . ltrim($dbopts["path"], '/') . "; sslmode=require";
+        self::$config["pdo"]["user"] = $dbopts["user"];
+        self::$config["pdo"]["password"] = $dbopts["pass"];
+    }
+        if (file_exists(self::$iniFile)) {
+            $mailConfig = parse_ini_file(self::$iniFile, true)["sendgrid"];
+            self::$config["email"]["apikey"] = $mailConfig ["apikey"];
+        } elseif (isset($_ENV["SENDGRID_APIKEY"])) {
+            self::$config["email"]["apikey"] = getenv('SENDGRID_APIKEY');
+        }
+        if (file_exists(self::$iniFile)) {
+            $pdfConfig = parse_ini_file(self::$iniFile, true)["hypdf"];
+            self::$config["pdf"]["hypdf-user"] = $pdfConfig["user"];
+
+        }elseif (isset($_ENV["HYPDF_USER"])) {
+            self::$config["pdf"]["hypdf-user"] = getenv(("HYPDF_USER"));
+        }
+        if (file_exists(self::$iniFile)) {
+            $pdfConfig = parse_ini_file(self::$iniFile, true)["hypdf"];
+            self::$config["pdf"]["hypdf-password"] = $pdfConfig["password"];}
+        elseif (isset($_ENV["HYPDF_PASSWORD"])) {
+            self::$config["pdf"]["hypdf-password"] = getenv(("HYPDF_PASSWORD"));
         }
     }
 
